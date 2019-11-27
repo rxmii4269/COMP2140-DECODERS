8c7d246909e3bb9c76762abd386785b0bce02644
TechWorth <lornemiller21@gmail.com>
2019-11-26 21:31:26 -0500

    Login Gui Working Now!!

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,34 +1,32 @@
+
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
 public class LoginController implements Initializable {
-    private LoginModel loginModel = new LoginModel();
+
+    LoginModel loginModel = new LoginModel();
     @FXML
     private Label dbstatus;
     @FXML
-    private TextField userName;
+    private TextField username;
     @FXML
     private PasswordField password;
     @FXML
-    private ComboBox<option> select;
+    private ComboBox<option> combobox;
     @FXML
-    private Button login_btn;
-    public void initialize(URL url, ResourceBundle rb){
+    private Button loginButton;
+
+    public void initialize (URL url, ResourceBundle rb){
         if(this.loginModel.isDBconnected()){
-            this.dbstatus.setText("Connected");
-
+            this.dbstatus.setText("Connected To DataBase");
         }else{
-            this.dbstatus.setText("Not Connected To DataBase");
+            this.dbstatus.setText("Not Connected To DAtaBAse");
         }
-        this.select.setItems(FXCollections.observableArrayList(option.values()));
+        this.combobox.setItems(FXCollections.observableArrayList(option.values()));
     }
-
 }
